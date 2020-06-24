7f9eae7be0c72eb8cc58bfa5bbf3349cf52f6e8f
claudiuvintila <claudiu.vintila@gmail.com>
2020-06-24 14:48:10 +0300

    Adapt rabbitmq util functions to our needs and make them reusable and portable

--- rabbitmq_util/publish.py
+++ rabbitmq_util/publish.py
@@ -1,32 +1,26 @@
 import pika
 import logging
+import os
+import sys
 
-logging.basicConfig(level=logging.DEBUG)
+def publish(body, exchange, exchange_type='direct', durable=True, delivery_mode=1):
+    logging.basicConfig(level=logging.WARN)
 
-credentials = pika.PlainCredentials('guest', 'guest')
-parameters =  pika.ConnectionParameters('localhost', credentials=credentials)
-connection = pika.BlockingConnection(parameters)
-channel = connection.channel()
-channel.exchange_declare(exchange="test_exchange", exchange_type="direct",
-                         passive=False, durable=True, auto_delete=False)
+    credentials = pika.PlainCredentials(os.environ['RABBITMQ_USER'], os.environ['RABBITMQ_PASS'])
+    parameters =  pika.ConnectionParameters(os.environ['RABBITMQ_HOST'], credentials=credentials)
+    connection = pika.BlockingConnection(parameters)
+    channel = connection.channel()
+    channel.exchange_declare(exchange=exchange, exchange_type=exchange_type,
+                             passive=False, durable=durable, auto_delete=False)
 
-print("Sending message to create a queue")
-channel.basic_publish('test_exchange', 'standard_key', 'queue:group',
-                      pika.BasicProperties(content_type='text/plain',
-                                           delivery_mode=1))
+    print("Sending text message: " + body)
+    sys.stdout.flush()
+    channel.basic_publish(exchange, 'standard_key', body,
+                          pika.BasicProperties(content_type='text/plain',
+                                               delivery_mode=delivery_mode))
 
-connection.sleep(5)
+    connection.close()
 
-print("Sending text message to group")
-channel.basic_publish('test_exchange', 'group_key', 'Message to group_key',
-                      pika.BasicProperties(content_type='text/plain',
-                                           delivery_mode=1))
 
-connection.sleep(5)
-
-print("Sending text message")
-channel.basic_publish('test_exchange', 'standard_key', 'Message to standard_key',
-                      pika.BasicProperties(content_type='text/plain',
-                                           delivery_mode=1))
-
-connection.close()
+if __name__ == '__main__':
+    publish('train_mnist', 'worker_exchange')
